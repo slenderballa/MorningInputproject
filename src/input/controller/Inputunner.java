15fb8a363b5ffe7c6a01e9a87c08c348ca20c424
Swenson <gswe4645@740s-pl-129.local>
2015-09-04 09:42:03 -0600

    Add project to repo.
    My “first” project added to Github everrrr!

---
+++ src/input/controller/Inputunner.java
@@ -0,0 +1,10 @@
+package input.controller;
+
+public class Inputunner
+{
+	public static void main (String [] args)
+	{
+		InputController myInputController = new InputController();
+		myInputController.start();
+	}
+}
